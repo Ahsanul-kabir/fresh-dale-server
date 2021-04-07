9e727a5aa25bbe57e890c218d8c1e5d8e43b796b
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-04-07 10:14:15 +0600

    home page partial impliment

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
 const express = require('express')
 const app = express()
-const port = 5055
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const cors = require('cors');
+require('dotenv').config()
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vl9uy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+// console.log(uri)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+
+  const eventCollection = client.db("freshDale").collection("products");
+
+  app.get('/events', (req, res) => {
+    eventCollection.find()
+      .toArray((err, items) => {
+        res.send(items)
+        // console.log('from database', items)
+      })
+  })
+
+  app.post('/addEvents', (req, res) => {
+    const newEvent = req.body;
+    console.log('adding new event: ', newEvent)
+    eventCollection.insertOne(newEvent)
+      .then(result => {
+        console.log('inserted count', result.insertedCount);
+        res.send(result.insertedCount > 0)
+      })
+  })
+
+  // app.delete('deleteEvent/:id', (req, res) => {
+  //   const id = ObjectID(req.params.id);
+  //   console.log('delete this', id);
+  //   eventCollection.findOneAndDelete({ _id: id })
+  //     .then(documents => res.send(!!documents.value))
+  // })
+  // console.log("Database Success");
+
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
